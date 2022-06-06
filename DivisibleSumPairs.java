1c04ffec9615a159c2d303dc093c7a62d22212a7
Sharon Baby <sharonoorakathu99@gmail.com>
2022-06-06 01:19:22 +0100

    practice

--- DivisibleSumPairs.java
+++ DivisibleSumPairs.java
@@ -1,2 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class DivisibleSumPairs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, k, count = 0;
+        List<Integer> params = Arrays.stream(sc.nextLine().split(" ")).map(i->Integer.parseInt(i)).collect(Collectors.toList());
+        List<Integer> arr = Arrays.stream(sc.nextLine().split(" ")).map(i->Integer.parseInt(i)).collect(Collectors.toList());
+        n = params.get(0);
+        k = params.get(1);
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                if((arr.get(i) + arr.get(j))%k == 0)
+                    count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
--- PickingNumbers.java
+++ PickingNumbers.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class PickingNumbers {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class PickingNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int loc = 1, max = 0;
+        sc.nextLine();
+        List<Integer> arr = Arrays.stream(sc.nextLine().split(" ")).map(i->Integer.parseInt(i)).collect(Collectors.toList());
+        Collections.sort(arr);
+        for (int i = 0; i < n-1; i=i+loc) {
+            int j = 1;
+            boolean flag = false;
+                while ((i + j != n) && (arr.get(i + j) - arr.get(i) <= 1)) {
+                    if (arr.get(i + j) - arr.get(i) == 1 && flag == false) {
+                        loc = j;
+                        flag = true;
+                    }
+                    j++;
+                }
+                if (j > max)
+                    max = j ;
+                if(flag == false)
+                    loc = j;
+
+        }
+        System.out.println(max);
+    }
 }
